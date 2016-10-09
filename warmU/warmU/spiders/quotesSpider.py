0dd5c7544d9127d448e1311c98628248e8f5042b
Kajo <ugorcak@gmx.ch>
2016-10-09 16:40:47 +0200

    added comments

--- warmU/warmU/spiders/quotesSpider.py
+++ warmU/warmU/spiders/quotesSpider.py
@@ -1,5 +1,7 @@
 import scrapy
 
+# Scrapy schedules the scrapy.Request objects returned by the start_requests method of the Spider. Upon receiving a response for each one, it instantiates <Response> objects and calls the callback method associated with the request(<parse> method) passing the response as argument.
+
 class QuotesSpider(scrapy.Spider):
     # name as unique identification of the spider:
     name = "quotes"
@@ -11,9 +13,11 @@ class QuotesSpider(scrapy.Spider):
             'http://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
+            # scrapy.Requests objects
             yield scrapy.Request(url=url, callback=self.parse)
 
     # parse method to handle the response downloaded for each request made:
+    # parse() parses the response, extracting the scraped data as dicts and finding new URLs to follow and creating new requests ( Request ) from them.
     def parse(self, response):
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page

50c3f12da8a93dd38002b583ba29b5dee17514ef
Kajo <ugorcak@gmx.ch>
2016-10-09 16:33:20 +0200

    generated html-copies

--- warmU/warmU/spiders/quotesSpider.py
+++ warmU/warmU/spiders/quotesSpider.py
@@ -1,8 +1,10 @@
 import scrapy
 
 class QuotesSpider(scrapy.Spider):
+    # name as unique identification of the spider:
     name = "quotes"
 
+    # return iteration of requests which the Spider will crawl from:
     def start_requests(self):
         urls = [
             'http://quotes.toscrape.com/page/1/',
@@ -11,6 +13,7 @@ class QuotesSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    # parse method to handle the response downloaded for each request made:
     def parse(self, response):
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
